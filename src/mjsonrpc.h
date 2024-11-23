a5a1605568873d2e85bcbc66bfe97f1f74cee029
sfxfs <2501683578@qq.com>
2024-11-23 15:22:44 +0800

    Enhance mjsonrpc error handling and documentation

--- src/mjsonrpc.h
+++ src/mjsonrpc.h
@@ -34,15 +34,21 @@
 #define JSON_RPC_2_0_INTERNAL_ERROR     -32693
 // -32000 to -32099 Reserved for implementation-defined server-errors.
 
+/**
+ * @brief mjrpc error code
+ */
 enum mjrpc_error_return
 {
     MJRPC_RET_OK,
     MJRPC_RET_ERROR_MEM_ALLOC_FAILED,
     MJRPC_RET_ERROR_NOT_FOUND,
-    MJRPC_RET_ERROR_EMPTY_REQUST,
+    MJRPC_RET_ERROR_EMPTY_REQUEST,
     MJRPC_RET_ERROR_PARSE_FAILED,
 };
 
+/**
+ * @brief jsonrpc callback function context
+ */
 typedef struct
 {
     void *data;
@@ -54,6 +60,9 @@ typedef cJSON *(*mjrpc_func)(mjrpc_ctx_t *context,
                                  cJSON *params,
                                  cJSON *id);
 
+/**
+ * @brief jsonrpc callback function
+ */
 struct mjrpc_cb
 {
     char *name;
@@ -61,26 +70,70 @@ struct mjrpc_cb
     void *arg;
 };
 
+/**
+ * @brief mjrpc handler
+ */
 typedef struct mjrpc_handler
 {
     int cb_count;
     struct mjrpc_cb *cb_array;
 } mjrpc_handler_t;
 
+/**
+ * @brief build a jsonrpc response with result
+ * @param result call result
+ * @param id client request id
+ * @return cjson pointer
+ */
 cJSON *mjrpc_response_ok(cJSON *result, cJSON *id);
 
+/**
+ * @brief build a jsonrpc response with error
+ * @param code error code
+ * @param message error message
+ * @param id client request id
+ * @return cjson pointer
+ */
 cJSON *mjrpc_response_error(int code, char *message, cJSON *id);
 
+/**
+ * @brief add a method to jsonrpc handler
+ * @param handler mjrpc handler
+ * @param function_pointer callback function
+ * @param method_name method name
+ * @param arg2func argument to callback function
+ * @return mjrpc_error_return
+ */
 int mjrpc_add_method(mjrpc_handler_t *handler,
                         mjrpc_func function_pointer,
                         char *method_name, void *arg2func);
 
+/**
+ * @brief delete a method from jsonrpc handler
+ * @param handler mjrpc handler
+ * @param method_name method name
+ * @return mjrpc_error_return
+ */
 int mjrpc_del_method(mjrpc_handler_t *handler, char *method_name);
 
+/**
+ * @brief process a string typed jsonrpc request
+ * @param handler mjrpc handler
+ * @param reqeust_str request string
+ * @param ret_code return code
+ * @return response string (need to be free)
+ */
 char *mjrpc_process_str(mjrpc_handler_t *handler,
                             const char *reqeust_str,
                             int *ret_code);
 
+/**
+ * @brief process a cjson typed jsonrpc request
+ * @param handler mjrpc handler
+ * @param request_cjson request cjson
+ * @param ret_code return code
+ * @return response cjson pointer (need to be free)
+ */
 cJSON *mjrpc_process_cjson(mjrpc_handler_t *handler,
                             cJSON *request_cjson,
                             int *ret_code);
