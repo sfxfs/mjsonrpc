711e7b884af24119e18cc8576da3cc304547baad
sfxfs <2501683578@qq.com>
2025-08-23 22:16:25 +0800

    Add Unity Test

---
+++ test/basic_test.c
@@ -0,0 +1,14 @@
+#include "unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_addition(void) {
+    TEST_ASSERT_EQUAL_INT(4, 2+2);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_addition);
+    return UNITY_END();
+}
