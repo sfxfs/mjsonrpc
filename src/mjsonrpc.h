918dee16770bb13f7f4fa4ab95211580753be9a5
sfxfs <2501683578@qq.com>
2025-08-30 20:14:37 +0800

    Del format check workflow

--- src/mjsonrpc.h
+++ src/mjsonrpc.h
@@ -26,8 +26,7 @@
 #define MJSONRPC_H_
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include "cJSON.h"
@@ -38,108 +37,108 @@ extern "C"
 #define JSON_RPC_CODE_METHOD_NOT_FOUND (-32601)
 #define JSON_RPC_CODE_INVALID_PARAMS (-32603)
 #define JSON_RPC_CODE_INTERNAL_ERROR (-32693)
-    // -32000 to -32099 Reserved for implementation-defined server-errors.
+// -32000 to -32099 Reserved for implementation-defined server-errors.
 
-    /**
-     * @brief mjrpc error code
-     */
-    enum mjrpc_error_return
-    {
-        MJRPC_RET_OK,
-        MJRPC_RET_OK_NOTIFICATION,
-        MJRPC_RET_ERROR_MEM_ALLOC_FAILED,
-        MJRPC_RET_ERROR_NOT_FOUND,
-        MJRPC_RET_ERROR_EMPTY_REQUEST,
-        MJRPC_RET_ERROR_NOT_OBJ_ARY,
-        MJRPC_RET_ERROR_PARSE_FAILED,
-        MJRPC_RET_ERROR_HANDLE_NOT_INITIALIZED,
-        MJRPC_RET_ERROR_INVALID_PARAM,
-    };
+/**
+ * @brief mjrpc error code
+ */
+enum mjrpc_error_return
+{
+    MJRPC_RET_OK,
+    MJRPC_RET_OK_NOTIFICATION,
+    MJRPC_RET_ERROR_MEM_ALLOC_FAILED,
+    MJRPC_RET_ERROR_NOT_FOUND,
+    MJRPC_RET_ERROR_EMPTY_REQUEST,
+    MJRPC_RET_ERROR_NOT_OBJ_ARY,
+    MJRPC_RET_ERROR_PARSE_FAILED,
+    MJRPC_RET_ERROR_HANDLE_NOT_INITIALIZED,
+    MJRPC_RET_ERROR_INVALID_PARAM,
+};
 
-    /**
-     * @brief jsonrpc callback function context
-     */
-    typedef struct
-    {
-        void* data;
-        int32_t error_code;
-        char* error_message;
-    } mjrpc_ctx_t;
+/**
+ * @brief jsonrpc callback function context
+ */
+typedef struct
+{
+    void* data;
+    int32_t error_code;
+    char* error_message;
+} mjrpc_ctx_t;
 
-    typedef cJSON* (*mjrpc_func)(mjrpc_ctx_t* context, cJSON* params, cJSON* id);
+typedef cJSON* (*mjrpc_func)(mjrpc_ctx_t* context, cJSON* params, cJSON* id);
 
-    /**
-     * @brief jsonrpc callback function
-     */
-    struct mjrpc_cb
-    {
-        uint32_t hash;
-        mjrpc_func function;
-        void* arg;
-    };
+/**
+ * @brief jsonrpc callback function
+ */
+struct mjrpc_cb
+{
+    uint32_t hash;
+    mjrpc_func function;
+    void* arg;
+};
 
-    /**
-     * @brief mjrpc handle
-     */
-    typedef struct mjrpc_handle
-    {
-        uint16_t cb_count;
-        struct mjrpc_cb* cb_array;
-    } mjrpc_handle_t;
+/**
+ * @brief mjrpc handle
+ */
+typedef struct mjrpc_handle
+{
+    uint16_t cb_count;
+    struct mjrpc_cb* cb_array;
+} mjrpc_handle_t;
 
-    /**
-     * @brief build a jsonrpc response with result
-     * @param result call result
-     * @param id client request id
-     * @return cjson pointer
-     */
-    cJSON* mjrpc_response_ok(cJSON* result, cJSON* id);
+/**
+ * @brief build a jsonrpc response with result
+ * @param result call result
+ * @param id client request id
+ * @return cjson pointer
+ */
+cJSON* mjrpc_response_ok(cJSON* result, cJSON* id);
 
-    /**
-     * @brief build a jsonrpc response with error
-     * @param code error code
-     * @param message error message
-     * @param id client request id
-     * @return cjson pointer
-     */
-    cJSON* mjrpc_response_error(int code, char* message, cJSON* id);
+/**
+ * @brief build a jsonrpc response with error
+ * @param code error code
+ * @param message error message
+ * @param id client request id
+ * @return cjson pointer
+ */
+cJSON* mjrpc_response_error(int code, char* message, cJSON* id);
 
-    /**
-     * @brief add a method to jsonrpc handle
-     * @param handle mjrpc handle
-     * @param function_pointer callback function
-     * @param method_name method name
-     * @param arg2func argument to callback function
-     * @return mjrpc_error_return
-     */
-    int mjrpc_add_method(mjrpc_handle_t* handle, mjrpc_func function_pointer,
-                         const char* method_name, void* arg2func);
+/**
+ * @brief add a method to jsonrpc handle
+ * @param handle mjrpc handle
+ * @param function_pointer callback function
+ * @param method_name method name
+ * @param arg2func argument to callback function
+ * @return mjrpc_error_return
+ */
+int mjrpc_add_method(mjrpc_handle_t* handle, mjrpc_func function_pointer,
+                     const char* method_name, void* arg2func);
 
-    /**
-     * @brief delete a method from jsonrpc handle
-     * @param handle mjrpc handle
-     * @param method_name method name if NULL, delete all methods
-     * @return mjrpc_error_return
-     */
-    int mjrpc_del_method(mjrpc_handle_t* handle, const char* method_name);
+/**
+ * @brief delete a method from jsonrpc handle
+ * @param handle mjrpc handle
+ * @param method_name method name if NULL, delete all methods
+ * @return mjrpc_error_return
+ */
+int mjrpc_del_method(mjrpc_handle_t* handle, const char* method_name);
 
-    /**
-     * @brief process a string typed jsonrpc request
-     * @param handle mjrpc handle
-     * @param reqeust_str request string
-     * @param ret_code return code
-     * @return response string (need to be free)
-     */
-    char* mjrpc_process_str(mjrpc_handle_t* handle, const char* reqeust_str, int* ret_code);
+/**
+ * @brief process a string typed jsonrpc request
+ * @param handle mjrpc handle
+ * @param reqeust_str request string
+ * @param ret_code return code
+ * @return response string (need to be free)
+ */
+char* mjrpc_process_str(mjrpc_handle_t* handle, const char* reqeust_str, int* ret_code);
 
-    /**
-     * @brief process a cjson typed jsonrpc request
-     * @param handle mjrpc handle
-     * @param request_cjson request cjson
-     * @param ret_code return code
-     * @return response cjson pointer (need to be free)
-     */
-    cJSON* mjrpc_process_cjson(mjrpc_handle_t* handle, const cJSON* request_cjson, int* ret_code);
+/**
+ * @brief process a cjson typed jsonrpc request
+ * @param handle mjrpc handle
+ * @param request_cjson request cjson
+ * @param ret_code return code
+ * @return response cjson pointer (need to be free)
+ */
+cJSON* mjrpc_process_cjson(mjrpc_handle_t* handle, const cJSON* request_cjson, int* ret_code);
 
 #ifdef __cplusplus
 }
