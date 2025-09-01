73e3fe1767cc23781dd64c21c055c3aba37c2c95
Xiao <39017816+sfxfs@users.noreply.github.com>
2025-09-01 22:16:47 +0800

    Integrated with hash table v2.0 Update (#2)
    Way of storage and index method changes to hash table, it can achieve fast search, insertion and deletion.

--- example/arg_to_function/arg_to_function.c
+++ example/arg_to_function/arg_to_function.c
@@ -5,9 +5,10 @@
 #include "mjsonrpc.h"
 
 // Define a simple JSON-RPC method
-cJSON* hello_world(mjrpc_func_ctx_t* context, cJSON* params, cJSON* id)
+cJSON* Count(mjrpc_func_ctx_t* context, cJSON* params, cJSON* id)
 {
-    cJSON* result = cJSON_CreateString("Hello, World!");
+    cJSON* result = cJSON_CreateString("Invoked!");
+    (*(int*) context->data)++;
     return result;
 }
 
@@ -16,8 +17,10 @@ int main()
     // Initialize mjrpc_handle_t
     mjrpc_handle_t* handle = mjrpc_create_handle(16);
 
+    int* count = malloc(sizeof(int));
+    *count = 0;
     // Add a method
-    mjrpc_add_method(handle, hello_world, "hello", NULL);
+    mjrpc_add_method(handle, Count, "hello", count);
 
     // Construct a JSON-RPC request
     const char* json_request = "{\"jsonrpc\":\"2.0\",\"method\":\"hello\",\"id\":1}";
@@ -27,6 +30,7 @@ int main()
     int result;
 
     json_response = mjrpc_process_str(handle, json_request, &result);
+    assert(*count == 1);
     // Assert that the result must be MJRPC_RET_OK
     assert(result == MJRPC_RET_OK);
     // Assert that the response contains "Hello, World!"
@@ -38,7 +42,6 @@ int main()
     mjrpc_del_method(handle, "hello");
 
     mjrpc_destroy_handle(handle);
-
 
     return 0;
 }
--- example/notif_req/notif_req.c
+++ example/notif_req/notif_req.c
@@ -5,7 +5,7 @@
 #include "mjsonrpc.h"
 
 // Define a simple JSON-RPC method
-cJSON* notify_func(mjrpc_ctx_t* context, cJSON* params, cJSON* id)
+cJSON* notify_func(mjrpc_func_ctx_t* context, cJSON* params, cJSON* id)
 {
     printf("notify_func called!\n");
     if (params && cJSON_IsString(params))
@@ -17,20 +17,21 @@ cJSON* notify_func(mjrpc_ctx_t* context, cJSON* params, cJSON* id)
 
 int main()
 {
-    mjrpc_handle_t handle = {0};
-    mjrpc_add_method(&handle, notify_func, "notify_method", NULL);
+    mjrpc_handle_t* handle = mjrpc_create_handle(16);
+    mjrpc_add_method(handle, notify_func, "notify_method", NULL);
 
     // Construct a JSON-RPC notification (without "id" field)
     const char* json_notify =
         "{\"jsonrpc\":\"2.0\",\"method\":\"notify_method\",\"params\":\"hello notify!\"}";
     int result;
-    char* json_response = mjrpc_process_str(&handle, json_notify, &result);
+    char* json_response = mjrpc_process_str(handle, json_notify, &result);
 
     // Notification type should return NULL, and result == MJRPC_RET_OK_NOTIFICATION
     assert(json_response == NULL);
     assert(result == MJRPC_RET_OK_NOTIFICATION);
     printf("notify request passed!\n");
 
-    mjrpc_del_method(&handle, "notify_method");
+    mjrpc_del_method(handle, "notify_method");
+    mjrpc_destroy_handle(handle);
     return 0;
 }
