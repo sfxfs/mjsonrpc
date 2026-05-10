7402b861143077e7b2a91613d8de35ff3468605e
sfxfs <sfxfs@users.noreply.github.com>
2026-05-10 08:39:03 +0000

    deploy: d11c60534e55aacebba27e6f8201440dd9565bed

---
+++ search/typedefs_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['mjrpc_5ferror_5flog_5ffunc_0',['mjrpc_error_log_func',['../group__memory__hooks.html#ga15f100956f60cf78f6eac175b105a0e9',1,'mjsonrpc.h']]],
+  ['mjrpc_5ffree_5ffunc_1',['mjrpc_free_func',['../group__memory__hooks.html#ga32c594346b574a7712f6a2f93d9c418c',1,'mjsonrpc.h']]],
+  ['mjrpc_5ffunc_2',['mjrpc_func',['../mjsonrpc_8h.html#a291322b35dbfbf6bfba742f97428a41f',1,'mjsonrpc.h']]],
+  ['mjrpc_5fhandle_5ft_3',['mjrpc_handle_t',['../mjsonrpc_8h.html#a47ae62a3d99e5d2b28b553fe0f1d1d92',1,'mjsonrpc.h']]],
+  ['mjrpc_5fmalloc_5ffunc_4',['mjrpc_malloc_func',['../group__memory__hooks.html#ga67675258e05e68090a3fadd1bd0cd064',1,'mjsonrpc.h']]],
+  ['mjrpc_5fstrdup_5ffunc_5',['mjrpc_strdup_func',['../group__memory__hooks.html#gad48de6b4d68a882d0e595b504ce49c70',1,'mjsonrpc.h']]]
+];
---
+++ search/variables_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['error_5fcode_0',['error_code',['../structmjrpc__func__ctx__t.html#a37c31cf01cfafcf538d3f2f287b93b6c',1,'mjrpc_func_ctx_t']]],
+  ['error_5fdata_1',['error_data',['../structmjrpc__func__ctx__t.html#af12a24db90fb202db4bf75f2f1470a7c',1,'mjrpc_func_ctx_t']]],
+  ['error_5fmessage_2',['error_message',['../structmjrpc__func__ctx__t.html#a6d5947fec617502b8da9504ed3edf9ea',1,'mjrpc_func_ctx_t']]]
+];
